08dfe0707b63a1eb3b1ca434add36ae30c47c6ea
Riley Calhoun <rileycalhoun0228@gmail.com>
2025-05-12 17:16:11 -0700

    Add config.yml: - Add "revert-after" setting

--- src/main/java/me/rileycalhoun/explosionreverter/ExplosionReverter.java
+++ src/main/java/me/rileycalhoun/explosionreverter/ExplosionReverter.java
@@ -1,5 +1,11 @@
 package me.rileycalhoun.explosionreverter;
 
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -9,13 +15,42 @@ import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class ExplosionReverter extends JavaPlugin implements Listener {
+
+    private int revertAfter;
 
     @Override
     public void onEnable() {
         long startTime = System.nanoTime();
+        getLogger().info("Initializing config files...");
+
+        YamlDocument configFile;
+        try {
+            configFile = YamlDocument.create(
+                    new File(getDataFolder(), ""),
+                    Objects.requireNonNull(getClass().getResourceAsStream("/config/config.yml")),
+                    GeneralSettings.DEFAULT,
+                    UpdaterSettings.builder()
+                            .setVersioning(new BasicVersioning("version"))
+                            .build(),
+                    LoaderSettings.builder()
+                            .setAutoUpdate(true)
+                            .build(),
+                    DumperSettings.DEFAULT
+            );
+        } catch (IOException e) {
+            getLogger().severe("Shutting down plugin. Could not load config: " + e.getMessage());
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        revertAfter = configFile.getInt("revert-after");
+
         getLogger().info("Registering listeners...");
         getServer().getPluginManager().registerEvents(this, this);
 
@@ -39,7 +74,7 @@ public class ExplosionReverter extends JavaPlugin implements Listener {
                 .runTaskLaterAsynchronously(
                         this,
                         () -> rebuildExplosion(blockList)
-                        , 20L * 30
+                        , 20L * revertAfter
                 );
     }
 
